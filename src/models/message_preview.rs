ac961e10c0536c8c4c593cb592f81c9221f6fcf6
Mathieu Cizo <maciz@outlook.fr>
2026-01-27 19:04:35 +0100

    feat: add MessagePreview model

---
+++ src/models/message_preview.rs
@@ -0,0 +1,19 @@
+use chrono::{DateTime, Utc};
+
+use crate::models::Role;
+
+#[derive(Debug, Clone)]
+pub struct MessagePreview {
+    pub session_id: String,
+    pub index: usize,
+    pub role: Role,
+    pub content_preview: String,
+    pub content_len: usize,
+    pub timestamp: DateTime<Utc>,
+}
+
+impl MessagePreview {
+    pub fn is_truncated(&self) -> bool {
+        self.content_preview.len() < self.content_len
+    }
+}
